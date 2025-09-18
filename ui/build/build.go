250560d920c68ddc34db6823f326a3e081e470cc
tb <ali609386314@gmail.com>
2025-09-18 17:58:00 +0800

    decrease BUILD_NUMBER length so ro.build.fingerprint be decreased

--- ui/build/build.go
+++ ui/build/build.go
@@ -77,7 +77,7 @@ func SetupOutDir(ctx Context, config Config) {
 		if username, ok = config.environ.Get("BUILD_USERNAME"); !ok {
 			ctx.Fatalln("Missing BUILD_USERNAME")
 		}
-		buildNumber = fmt.Sprintf("eng.%.6s.%s", username, time.Now().Format("20060102.150405" /* YYYYMMDD.HHMMSS */))
+		buildNumber = fmt.Sprintf("%s", time.Now().Format("20060102.150405" /* YYYYMMDD.HHMMSS */))
 		writeValueIfChanged(ctx, config, config.OutDir(), "file_name_tag.txt", username)
 	}
 	// Write the build number to a file so it can be read back in
